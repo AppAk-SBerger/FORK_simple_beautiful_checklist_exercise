15a11caf531aa96ae5d64cc6e5f434a1912c3c95
SBerger_DEV <teebeutel.de@gmail.com>
2025-03-04 14:21:48 +0100

    feat: outsource MainApp; update MockDatabase to SharedPreferencesRepository

---
+++ lib/main_app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_beautiful_checklist_exercise/features/splash/splash_screen.dart';
+import 'package:simple_beautiful_checklist_exercise/home_screen.dart';
+import 'package:simple_beautiful_checklist_exercise/shared/database_repository.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({
+    super.key,
+    required this.repository,
+  });
+
+  final DatabaseRepository repository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: /*ThemeData(
+        brightness: Brightness.light,
+        textTheme: GoogleFonts.robotoMonoTextTheme(Theme.of(context).textTheme),
+      ),
+      darkTheme:*/
+          ThemeData(
+        brightness: Brightness.dark,
+        textTheme: GoogleFonts.robotoMonoTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme,
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      title: 'Checklisten App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/home': (context) => HomeScreen(
+              repository: repository,
+            ),
+      },
+    );
+  }
+}
